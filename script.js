fe9d1260266bbe0b8aef04f0166b7defea228e01
Naveena18BM034 <84767263+Naveena18BM034@users.noreply.github.com>
2026-06-19 21:32:47 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const form = document.getElementById("contactForm");
+const successMessage =
+document.getElementById("successMessage");
+
+form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const data = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    };
+
+    try {
+
+        const response = await fetch(
+            "http://localhost:5000/contact",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            }
+        );
+
+        const result = await response.json();
+
+        successMessage.textContent =
+            result.message;
+
+        form.reset();
+
+    } catch (error) {
+
+        successMessage.textContent =
+            "Failed to send message";
+    }
+});
